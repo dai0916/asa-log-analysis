24b26bb7a4543953075efa6c607fd6b127bc1881
Daisaku Yamamoto <daisaku0916@gmail.com>
2016-03-01 01:39:41 +0900

    add DiscoverTheOntime.py.

---
+++ DiscoverTheOntime.py
@@ -0,0 +1,131 @@
+#! /usr/bin/env python
+
+#
+# DiscoverTheOntime.py --- Discover the ontime with users from ASA syslog
+#
+import sys
+import re
+import datetime
+import itertools
+
+html = """
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+    google.load('visualization', '1', {{'packages':['timeline']}});
+    google.setOnLoadCallback(drawChart);
+
+    function drawChart() {{
+        var data = new google.visualization.DataTable();
+        data.addColumn({{ type: 'string', id: 'Number' }});
+        data.addColumn({{ type: 'string', id: 'Name' }});
+        data.addColumn({{ type: 'date', id: 'Start' }});
+        data.addColumn({{ type: 'date', id: 'End' }});
+        data.addRows([
+            {0}
+        ]);
+
+        var chart = new google.visualization.Timeline(document.getElementById('chart_div'));
+
+        var options = {{
+            timeline: {{
+                showRowLabels: false,
+            }}
+        }};
+
+        chart.draw(data, options);
+      }}
+</script>
+</head>
+<body>
+    <div id="chart_div" style="height:100%"></div>
+</body>
+</html>
+"""[1:-1]
+
+def toDateTime (log_datetime):
+    date = (log_datetime.split(' '))[3:]
+    return datetime.datetime.strptime (' '.join (date), "%b %d %H:%M:%S %Z")
+
+def extract_start_session_from_log(line):
+    result = None
+    if re.search (r'%ASA-[^:]+?-113039:', line):
+        sep = line.rstrip().split (': ')
+        result = {}
+        result["terminate log"] = None
+        result["line"]          = line.rstrip()
+        #
+        result["Message"]  = sep[2]
+        result["Class"]    = sep[1]
+        result["DateTime"] = toDateTime (sep[0])
+        #
+        matches = re.match (r'Group <(.*?)> User <(.*?)> IP <(.*?)>', sep[2])
+        result["Group"]    = matches.group(1)
+        result["User"]     = matches.group(2)
+        result["IP"]       = matches.group(3)
+    return result
+
+def extract_terminate_session_from_log(line):
+    result = None
+    if re.search (r'%ASA-[^:]+?-113019:', line):
+        sep = line.rstrip().split (': ')
+        result = {}
+        result["start log"]     = None
+        result["line"]          = line.rstrip()
+        #
+        result["Message"]  = ': '.join (sep[2:])
+        result["Class"]    = sep[1]
+        result["DateTime"] = toDateTime (sep[0])
+        #
+        matches = re.match (r'Group = (.*?), Username = (.*?), IP = (.*?), Session disconnected\. Session Type: (.*?), Duration: (.*?), ', result["Message"])
+        result["Group"]         = matches.group(1)
+        result["User"]          = matches.group(2)
+        result["IP"]            = matches.group(3)
+        result["SessionType"]   = matches.group(4)
+        result["Duration"]      = matches.group(5)
+    return result
+
+start_session_logs = []
+terminate_session_logs = []
+
+for line in sys.stdin:
+    start_session = extract_start_session_from_log (line)
+    if start_session:
+        start_session_logs.append (start_session)
+    #
+    terminate_session = extract_terminate_session_from_log (line)
+    if terminate_session:
+        terminate_session_logs.append (terminate_session)
+
+#
+for start in start_session_logs:
+    term = next(itertools.ifilter(lambda it:it["DateTime"] >= start["DateTime"] and it["IP"] == start["IP"], terminate_session_logs), None)
+    if term:
+        start["terminate log"] = term
+        term["start log"] = start
+
+
+for item in itertools.ifilter(lambda it:it["terminate log"] == None, start_session_logs):
+    print >> sys.stderr, "This record,\n{0}\n has no relationship!".format (item["line"])
+for item in itertools.ifilter(lambda it:it["start log"] == None, terminate_session_logs):
+    print >> sys.stderr, "This record,\n{0}\n has no relationship!".format (item["line"])
+
+buf = []
+for num, item in enumerate (start_session_logs):
+    st = item
+    tm = item["terminate log"]
+    text = "[ '{0}', '{1}'".format (1+num, st["User"])
+    text = text + ", new Date({0})".format (st["DateTime"].strftime ('%Y, %m, %d, %H, %M, %S'))
+    if tm:
+        text = text + ", new Date({0})".format (tm["DateTime"].strftime ('%Y, %m, %d, %H, %M, %S'))
+        text = text + "],"
+        buf.append (text)
+
+buf[-1] = buf[-1].rstrip(',')
+
+#print start_and_term_relationship
+
+datarows = "\n            ".join (buf)
+print html.format(datarows)
